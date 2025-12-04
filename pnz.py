bd582916998aa2cb410bca6a2cd1f94ac82ecdd5
pratikwankhede1905 <pratikwankhede1905@gmail.com>
2025-12-04 13:49:37 +0530

    Add files via upload

---
+++ pnz.py
@@ -0,0 +1,11 @@
+def num(n):
+    if n > 1:
+        print("number is positive")
+
+    elif n < 0:
+        print("number is negative")
+
+    else:
+        print("number is zero")
+n = int(input("Enter Your Number: "))
+num(n)
